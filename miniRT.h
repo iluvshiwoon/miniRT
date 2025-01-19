4e85068f38539bd6d6a3071cca0837b4b3f50b4b
iluvshiwoon <sntcillian@gmail.com>
2025-01-19 22:58:47 +0000

    add basic memory function

--- miniRT.h
+++ miniRT.h
@@ -6,7 +6,7 @@
 /*   By: kgriset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 20:37:42 by kgriset           #+#    #+#             */
-/*   Updated: 2025/01/19 20:14:44 by kgriset          ###   ########.fr       */
+/*   Updated: 2025/01/19 22:57:37 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,14 @@
 # include <fcntl.h>
 # include "42_MyLibC/mylibc.h"
 
+
+typedef struct s_rt {
+    t_link_list * graphic_heap;
+    t_link_list * current_heap;
+} t_rt;
+
+// wrap_malloc.c
+void	*wrap_malloc(t_rt *rt, size_t size);
+void free_heap(t_rt *rt);
+t_link_list *init_alloc(t_link_list **list);
 #endif
