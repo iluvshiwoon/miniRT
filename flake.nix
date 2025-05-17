dcaa716b7192e563a4a4efa8230a721a1c3f869c
iluvshiwoon <sntcillian@gmail.com>
2025-05-17 14:18:35 +0800

    flake for c dev-env

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: 
+        let
+          stdenv = pkgs.clangStdenv;
+        in
+          {
+        default = pkgs.mkShell.override
+          {
+            inherit stdenv;
+          }
+          {
+            packages = with pkgs; [
+              clang-tools
+              # cmake
+              # codespell
+              # conan
+              # cppcheck
+              # doxygen
+              # gtest
+              # lcov
+              # vcpkg
+              # vcpkg-tool
+            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          };
+        env = {
+            CLANGD_FLAGS = "--query-driver=${pkgs.lib.getExe stdenv.cc}";
+        };
+      });
+    };
+}
