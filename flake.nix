e72d283e4b3ebe5ffbebbde08ca2a588db1db227
shuen <sntcillian@gmail.com>
2025-05-28 04:57:13 -0400

    modify flake for minilibx compilation

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
-
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
-
   outputs = inputs: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSupportedSystem = f:
@@ -22,16 +20,16 @@
         {
           packages = with pkgs;
             [
+              gcc
               clang-tools
-              # cmake
-              # codespell
-              # conan
-              # cppcheck
-              # doxygen
-              # gtest
-              # lcov
-              # vcpkg
-              # vcpkg-tool
+              libbsd
+              # X11 libraries and development headers
+              xorg.libX11
+              xorg.libX11.dev
+              # X11 extensions (required for XShm.h and others)
+              xorg.libXext
+              xorg.libXext.dev
+              xorg.xorgproto  # Contains X11 protocol headers
             ]
             ++ (
               if system == "aarch64-darwin"
@@ -40,12 +38,20 @@
             );
           env = {
             CLANGD_FLAGS = "--query-driver=${pkgs.lib.getExe stdenv.cc}";
+            # Make X11 libraries and headers available
+            CFLAGS = "-I${pkgs.xorg.libX11.dev}/include -I${pkgs.xorg.xorgproto}/include";
+            LDFLAGS = "-L${pkgs.xorg.libX11}/lib -L${pkgs.xorg.libXext}/lib -lX11 -lXext";
           };
-          # shellHook = ''
-          #   if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
-          #   source "$GHOSTTY_RESOURCES_DIR"/shell-integration/zsh/ghostty-integration
-          #   fi
-          # '';
+          shellHook = ''
+            echo "X11 development environment loaded"
+            echo "CFLAGS and LDFLAGS are set for X11"
+            echo "You can now compile with: gcc \$CFLAGS \$LDFLAGS your_program.c"
+            
+            # Export X11 path for minilibX configure script (includes extensions)
+            export NIX_X11_INCLUDE="${pkgs.xorg.xorgproto}/include"
+            echo "X11 headers (including extensions) available at: $NIX_X11_INCLUDE"
+            echo "libX11 headers available at: ${pkgs.xorg.libX11.dev}/include"
+          '';
         };
     });
   };
