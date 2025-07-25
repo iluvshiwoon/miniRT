49783a71026b42c1d102c7c9470f43bf69554f8b
Killian Griset <kgriset@f4r11s9.clusters.42paris.fr>
2025-07-25 12:54:46 +0200

    norm

--- parsing/element/element_utils.c
+++ parsing/element/element_utils.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 09:38:28 by gschwand          #+#    #+#             */
-/*   Updated: 2025/07/18 17:40:38 by kgriset          ###   ########.fr       */
+/*   Updated: 2025/07/25 12:53:24 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@ void	check_size_tab(t_rt *rt, char **tab)
 
 	i = 0;
 	if (!tab)
-        	exit_error(rt, "Error: Invalid number of arguments");
-	while (tab[i]) i++;
+		exit_error(rt, "Error: Invalid number of arguments");
+	while (tab[i])
+		i++;
 	if (i != 3)
-        	exit_error(rt, "Error: Invalid number of arguments");
+		exit_error(rt, "Error: Invalid number of arguments");
 }
 
-void	check_color(t_rt * rt,char *line)
+void	check_color(t_rt *rt, char *line)
 {
 	int	i;
 
@@ -32,12 +33,12 @@ void	check_color(t_rt * rt,char *line)
 	while (line[i])
 	{
 		if (!ft_isdigit(line[i]))
-            exit_error(rt, "Error: Invalid color");
+			exit_error(rt, "Error: Invalid color");
 		i++;
 	}
 }
 
-void	check_vec(t_rt * rt, char *line)
+void	check_vec(t_rt *rt, char *line)
 {
 	int	i;
 	int	point;
@@ -47,13 +48,13 @@ void	check_vec(t_rt * rt, char *line)
 	while (line[i])
 	{
 		if (!ft_isdigit(line[i]) && i == 0 && line[i] != '-')
-            exit_error(rt,"Error: Invalid vector");
+			exit_error(rt, "Error: Invalid vector");
 		else if (line[i] == '.')
 			point++;
 		i++;
 	}
 	if (point > 1)
-        exit_error(rt,"Error: Invalid vector");
+		exit_error(rt, "Error: Invalid vector");
 }
 
 t_vec	parse_color(t_rt *rt, char *line)
@@ -61,42 +62,33 @@ t_vec	parse_color(t_rt *rt, char *line)
 	t_vec	color;
 	char	**tab;
 
-    tab = rt_ft_split(rt,line, ',');
-	check_size_tab(rt,tab);
-    check_color(rt, tab[0]);
-    color.x = ft_atoi(tab[0]);
-    check_color(rt, tab[1]);
+	tab = rt_ft_split(rt, line, ',');
+	check_size_tab(rt, tab);
+	check_color(rt, tab[0]);
+	color.x = ft_atoi(tab[0]);
+	check_color(rt, tab[1]);
 	color.y = ft_atoi(tab[1]);
-    check_color(rt , tab[2]);
+	check_color(rt, tab[2]);
 	color.z = ft_atoi(tab[2]);
-    if (color.x < 0 || color.y < 0 || color.z < 0 || color.x > 255
+	if (color.x < 0 || color.y < 0 || color.z < 0 || color.x > 255
 		|| color.y > 255 || color.z > 255)
-exit_error(rt, "Error: Invalid color");
+		exit_error(rt, "Error: Invalid color");
 	return (color);
 }
 
 t_vec	parse_vec(t_rt *rt, char *line)
 {
-    t_vec vec;
+	t_vec	vec;
 	char	**tab;
 
-    vec = (t_vec){};
-    tab = rt_ft_split(rt, line, ',');
+	vec = (t_vec){};
+	tab = rt_ft_split(rt, line, ',');
 	check_size_tab(rt, tab);
-    check_vec(rt, tab[0]);
+	check_vec(rt, tab[0]);
 	vec.x = ft_atoi_double(tab[0]);
-    check_vec(rt, tab[1]);
+	check_vec(rt, tab[1]);
 	vec.y = ft_atoi_double(tab[1]);
-    check_vec(rt, tab[2]); 
-    vec.z = ft_atoi_double(tab[2]);
+	check_vec(rt, tab[2]);
+	vec.z = ft_atoi_double(tab[2]);
 	return (vec);
-}
-
-void	print_scene(t_rt *rt)
-{
-	int	i;
-
-	i = -1;
-    while (++i < rt->scene.total_objects)
-	printf("%s\n", rt->scene.objects[i].string);
 }
--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:48:23 by gschwand          #+#    #+#             */
-/*   Updated: 2025/04/02 15:26:37 by gschwand         ###   ########.fr       */
+/*   Updated: 2025/07/25 12:53:37 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,4 @@ void    parsing_minirt(t_rt *rt,char *namefile)
 
 	file = open_file(rt, namefile);
 	alloc_rt(rt, file);
-	print_scene(rt);
 }
