50ae589c4a236ba0a8288eaa97898b7507f7229a
iluvshiwoon <sntcillian@gmail.com>
2025-08-20 10:09:26 +0200

    norm

--- parsing/element/cylinder.c
+++ parsing/element/cylinder.c
@@ -12,72 +12,69 @@
 
 #include "../../miniRT.h"
 
-static char	*append_cylinder_dims(t_rt *rt, char *r_value, \
-    t_cylinder *cylinder)
+static char	*append_cylinder_dims(t_rt *rt, char *r_value, t_cylinder *cylinder)
 {
-    char    dest[24 + 1];
+	char	dest[24 + 1];
 
-    ft_memset(dest, 0, sizeof(dest));
-    fpconv_dtoa(cylinder->radius, dest);
-    r_value = rt_ft_strjoin(rt, r_value, dest);
-    ft_memset(dest, 0, sizeof(dest));
-    r_value = rt_ft_strjoin(rt, r_value, " ");
-    fpconv_dtoa(cylinder->height, dest);
-    return (rt_ft_strjoin(rt, r_value, dest));
+	ft_memset(dest, 0, sizeof(dest));
+	fpconv_dtoa(cylinder->radius, dest);
+	r_value = rt_ft_strjoin(rt, r_value, dest);
+	ft_memset(dest, 0, sizeof(dest));
+	r_value = rt_ft_strjoin(rt, r_value, " ");
+	fpconv_dtoa(cylinder->height, dest);
+	return (rt_ft_strjoin(rt, r_value, dest));
 }
 
-static char	*append_optional_maps_cy(t_rt *rt, char *r_value, \
-    const struct s_object object)
+static char	*append_optional_maps_cy(t_rt *rt, char *r_value,
+		const struct s_object object)
 {
-    if (object.normal_map_path || object.texture_map_path || \
-        object.texture_scale.x != 1.0 || object.texture_scale.y != 1.0)
-    {
-        r_value = rt_ft_strjoin(rt, r_value, " ");
-        if (object.normal_map_path)
-            r_value = rt_ft_strjoin(rt, r_value, object.normal_map_path);
-        else
-            r_value = rt_ft_strjoin(rt, r_value, ".");
-        r_value = rt_ft_strjoin(rt, r_value, " ");
-        if (object.texture_map_path)
-            r_value = rt_ft_strjoin(rt, r_value, object.texture_map_path);
-        else
-            r_value = rt_ft_strjoin(rt, r_value, ".");
-        r_value = rt_ft_strjoin(rt, r_value, " ");
-        r_value = rt_ft_strjoin(rt, r_value, \
-            vec_toa(rt, object.texture_scale));
-    }
-    return (r_value);
+	if (object.normal_map_path || object.texture_map_path
+		|| object.texture_scale.x != 1.0 || object.texture_scale.y != 1.0)
+	{
+		r_value = rt_ft_strjoin(rt, r_value, " ");
+		if (object.normal_map_path)
+			r_value = rt_ft_strjoin(rt, r_value, object.normal_map_path);
+		else
+			r_value = rt_ft_strjoin(rt, r_value, ".");
+		r_value = rt_ft_strjoin(rt, r_value, " ");
+		if (object.texture_map_path)
+			r_value = rt_ft_strjoin(rt, r_value, object.texture_map_path);
+		else
+			r_value = rt_ft_strjoin(rt, r_value, ".");
+		r_value = rt_ft_strjoin(rt, r_value, " ");
+		r_value = rt_ft_strjoin(rt, r_value, vec_toa(rt, object.texture_scale));
+	}
+	return (r_value);
 }
 
 static void	parse_cylinder_optional(t_rt *rt, char **tab, int *id)
 {
-    if (tab[6])
-    {
-        if (ft_strncmp(tab[6], ".", 2) != 0)
-            rt->scene.objects[*id].specular = vec_mult(1.0 / 255, \
-                parse_color(rt, tab[6]));
-        if (tab[7])
-        {
-            if (ft_strncmp(tab[7], ".", 2) != 0)
-                rt->scene.objects[*id].shininess = \
-                    ft_atoi_double(tab[7]);
-            if (tab[8])
-            {
-                if (ft_strncmp(tab[8], ".", 2) != 0)
-                    rt->scene.objects[*id].normal_map_path = \
-                        rt_ft_strdup(rt, tab[8]);
-                if (tab[9])
-                {
-                    if (ft_strncmp(tab[9], ".", 2) != 0)
-                        rt->scene.objects[*id].texture_map_path = \
-                            rt_ft_strdup(rt, tab[9]);
-                    if (tab[10])
-                        rt->scene.objects[*id].texture_scale = \
-                            parse_vec(rt, tab[10]);
-                }
-            }
-        }
-    }
+	if (tab[6])
+	{
+		if (ft_strncmp(tab[6], ".", 2) != 0)
+			rt->scene.objects[*id].specular = vec_mult(1.0 / 255,
+					parse_color(rt, tab[6]));
+		if (tab[7])
+		{
+			if (ft_strncmp(tab[7], ".", 2) != 0)
+				rt->scene.objects[*id].shininess = ft_atoi_double(tab[7]);
+			if (tab[8])
+			{
+				if (ft_strncmp(tab[8], ".", 2) != 0)
+					rt->scene.objects[*id].normal_map_path = rt_ft_strdup(rt,
+							tab[8]);
+				if (tab[9])
+				{
+					if (ft_strncmp(tab[9], ".", 2) != 0)
+						rt->scene.objects[*id].texture_map_path = rt_ft_strdup(rt,
+								tab[9]);
+					if (tab[10])
+						rt->scene.objects[*id].texture_scale = parse_vec(rt,
+								tab[10]);
+				}
+			}
+		}
+	}
 }
 
 void	rotate_cylinder_local(t_rt *rt, int id, t_rvec rvec)
@@ -94,8 +91,7 @@ void	rotate_cylinder_local(t_rt *rt, int id, t_rvec rvec)
 	if (fabs(rvec.pitch) > 1e-6)
 	{
 		cl.pitch_rot = create_rotation_axis(cl.right, rvec.pitch);
-		cl.cyl->dir = normalize(mat3_multiply_vec(cl.pitch_rot,
-					cl.cyl->dir));
+		cl.cyl->dir = normalize(mat3_multiply_vec(cl.pitch_rot, cl.cyl->dir));
 	}
 	if (fabs(rvec.yaw) > 1e-6)
 	{
@@ -105,7 +101,7 @@ void	rotate_cylinder_local(t_rt *rt, int id, t_rvec rvec)
 	rt->scene.objects[id].string = rt->scene.objects[id].display_string(rt,
 			rt->scene.objects[id]);
 	if (rvec.roll == 0)
-        atomic_store(&rt->state.re_render_scene, true);
+		atomic_store(&rt->state.re_render_scene, true);
 }
 
 void	translate_cylinder(t_rt *rt, int id, t_vec vec)
@@ -116,32 +112,32 @@ void	translate_cylinder(t_rt *rt, int id, t_vec vec)
 	cylinder->origin = vec_plus(cylinder->origin, vec);
 	rt->scene.objects[id].string = rt->scene.objects[id].display_string(rt,
 			rt->scene.objects[id]);
-    atomic_store(&rt->state.re_render_scene, true);
+	atomic_store(&rt->state.re_render_scene, true);
 }
 
 char	*string_cylinder(t_rt *rt, const struct s_object object)
 {
 	char		*r_value;
 	t_cylinder	*cylinder;
+		char dest2[24 + 1];
 
 	cylinder = object.obj;
 	r_value = rt_ft_strjoin(rt, "cy  ", vec_toa(rt, cylinder->origin));
 	r_value = rt_ft_strjoin(rt, r_value, "  ");
 	r_value = rt_ft_strjoin(rt, r_value, vec_toa(rt, cylinder->dir));
 	r_value = rt_ft_strjoin(rt, r_value, " ");
-    r_value = append_cylinder_dims(rt, r_value, cylinder);
+	r_value = append_cylinder_dims(rt, r_value, cylinder);
 	r_value = rt_ft_strjoin(rt, r_value, "  ");
 	r_value = rt_ft_strjoin(rt, r_value, color_toa(rt, object.albedo));
 	r_value = rt_ft_strjoin(rt, r_value, " ");
 	r_value = rt_ft_strjoin(rt, r_value, color_toa(rt, object.specular));
 	r_value = rt_ft_strjoin(rt, r_value, " ");
-    {
-        char dest2[24 + 1];
-        ft_memset(dest2, 0, sizeof(dest2));
-        fpconv_dtoa(object.shininess, dest2);
-        r_value = rt_ft_strjoin(rt, r_value, dest2);
-    }
-    r_value = append_optional_maps_cy(rt, r_value, object);
+	{
+		ft_memset(dest2, 0, sizeof(dest2));
+		fpconv_dtoa(object.shininess, dest2);
+		r_value = rt_ft_strjoin(rt, r_value, dest2);
+	}
+	r_value = append_optional_maps_cy(rt, r_value, object);
 	return (r_value);
 }
 
@@ -153,8 +149,7 @@ void	fill_cy(t_rt *rt, t_cylinder *cylinder, int id)
 	rt->scene.objects[id].display_string = &string_cylinder;
 	rt->scene.objects[id].rotate = &rotate_cylinder_local;
 	rt->scene.objects[id].translate = &translate_cylinder;
-	rt->scene.objects[id].string = string_cylinder(rt, \
-			rt->scene.objects[id]);
+	rt->scene.objects[id].string = string_cylinder(rt, rt->scene.objects[id]);
 	// Initialize material properties with default values
 	rt->scene.objects[id].checkerboard = false; // Default no checkerboard
 }
@@ -185,7 +180,7 @@ void	parse_cylinder(t_rt *rt, char *line, int *id)
 		rt->scene.objects[*id].shininess = 32.0;
 		rt->scene.objects[*id].texture_scale = (t_vec){1.0, 1.0, 1.0};
 		rt->scene.objects[*id].checkerboard = false;
-        parse_cylinder_optional(rt, tab, id);
+		parse_cylinder_optional(rt, tab, id);
 		fill_cy(rt, cylinder, *id);
 		(*id)++;
 		return ;
